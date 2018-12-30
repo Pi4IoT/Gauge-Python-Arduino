e4ac80160cd7d1f4277c00a2aaed0a546aa6dd70
Pi4IoT <36192933+Pi4IoT@users.noreply.github.com>
2018-12-30 23:02:03 +0100

    Add files via upload

---
+++ Arduino/poti.ino
@@ -0,0 +1,14 @@
+/* Pi4IoT 30.12.2018  */
+ 
+void setup() {
+  Serial.begin(115200);
+}
+     
+void loop() {
+  if (Serial.available()) {
+    if (Serial.read() == 'p'){
+      Serial.print(analogRead(A0));
+      Serial.print("\n");
+    }
+  }
+}
